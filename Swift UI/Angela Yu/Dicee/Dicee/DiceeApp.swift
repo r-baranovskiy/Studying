b6cadadc8e85727165e44d3d85494d3633c6fcc8
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-15 15:11:34 +0300

    The app Dicee has been completed

---
+++ Swift UI/Angela Yu/Dicee/Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Кащенко on 15.01.23.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
