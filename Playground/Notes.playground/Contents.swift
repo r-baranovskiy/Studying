be6e2e1e953bffacba01e7e139f03317334c8e3c
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-19 02:34:21 +0300

    Updated README guide

--- Playground/Notes.playground/Contents.swift
+++ Playground/Notes.playground/Contents.swift
@@ -67,13 +67,13 @@ import UIKit
 
 /*
  struct Resolution {
-     var width = 0
-     var height = 0
+ var width = 0
+ var height = 0
  }
-
+ 
  let constantStruct = Resolution()
  //constantStruct.width = 2 // Cannot assign to property: 'constantStruct' is a 'let' constant
-
+ 
  var varStruct = Resolution()
  varStruct.width = 2
  print(varStruct.width) // 2
@@ -84,18 +84,18 @@ import UIKit
 
 /*
  class Human {
-     var name = "Ruslan"
-     var surname = "Chaykin"
+ var name = "Ruslan"
+ var surname = "Chaykin"
  }
-
+ 
  let constantClass = Human()
  constantClass.name = "Andrey"
  print(constantClass.name) // Andrey
-
+ 
  var varClass = Human()
  varClass.name = "Kostya"
  print(varClass.name) // Kostya
-
+ 
  varClass = constantClass
  print(varClass.name) //Andrey
  */
@@ -106,142 +106,182 @@ import UIKit
 
 /*
  var property: Type {
-     get {
-         code
-     }
-     set(value) {
-         code
-     }
+ get {
+ code
+ }
+ set(value) {
+ code
+ }
  }
  */
 
 /*
-struct RentalCar {
-    var costPerDay: Double
-    
-    var costPerWeek: Double {
-        get {
-            costPerDay * 7.0
-        }
-        
-        set {
-            costPerDay = newValue / 7.0
-        }
-    }
-}
-
-var mazda = RentalCar(costPerDay: 20.0)
-print(mazda.costPerWeek) // 140
-print(mazda.costPerDay) // 20
-
-mazda.costPerDay = 50
-print(mazda.costPerWeek) // 350
-
-mazda.costPerWeek = 140
-print(mazda.costPerWeek) // 140.0
-print(mazda.costPerDay) // 20.0
-*/
+ struct RentalCar {
+ var costPerDay: Double
+ 
+ var costPerWeek: Double {
+ get {
+ costPerDay * 7.0
+ }
+ 
+ set {
+ costPerDay = newValue / 7.0
+ }
+ }
+ }
+ 
+ var mazda = RentalCar(costPerDay: 20.0)
+ print(mazda.costPerWeek) // 140
+ print(mazda.costPerDay) // 20
+ 
+ mazda.costPerDay = 50
+ print(mazda.costPerWeek) // 350
+ 
+ mazda.costPerWeek = 140
+ print(mazda.costPerWeek) // 140.0
+ print(mazda.costPerDay) // 20.0
+ */
 
 //MARK: - Наблюдатели свойств willSet и didSet
 
 /*
-class StepCounter {
-    var totalSteps: Int = 0 {
-        willSet(newTotalSteps) {
-            print("Вот-вот значение будет равно \(newTotalSteps)")
-        }
-        didSet {
-            if totalSteps > oldValue  {
-                print("Добавлено \(totalSteps - oldValue) шагов")
-            }
-        }
-    }
-}
-let stepCounter = StepCounter()
-stepCounter.totalSteps = 200
-// Вот-вот значение будет равно 200
-// Добавлено 200 шагов
-stepCounter.totalSteps = 360
-// Вот-вот значение будет равно 360
-// Добавлено 160 шагов
-stepCounter.totalSteps = 896
-// Вот-вот значение будет равно 896
-// Добавлено 536 шагов
-*/
+ class StepCounter {
+ var totalSteps: Int = 0 {
+ willSet(newTotalSteps) {
+ print("Вот-вот значение будет равно \(newTotalSteps)")
+ }
+ didSet {
+ if totalSteps > oldValue  {
+ print("Добавлено \(totalSteps - oldValue) шагов")
+ }
+ }
+ }
+ }
+ let stepCounter = StepCounter()
+ stepCounter.totalSteps = 200
+ // Вот-вот значение будет равно 200
+ // Добавлено 200 шагов
+ stepCounter.totalSteps = 360
+ // Вот-вот значение будет равно 360
+ // Добавлено 160 шагов
+ stepCounter.totalSteps = 896
+ // Вот-вот значение будет равно 896
+ // Добавлено 536 шагов
+ */
 
 
 
 // MARK: - Type Properties (Свойства типа)
 
 /*
-class SomeClass {
-    
-    static let someProperty: Int = 10
-    
-    func someFunc() {
-        print(#function)
-    }
-    
-    static func staticFunc() {
-        print(#function)
-    }
-    
-    class func classFunc() {
-        print(#function)
-    }
-    
-}
-
-let someClass = SomeClass()
-
-someClass.someFunc() // someFunc()
-
-SomeClass.staticFunc() // staticFunc()
-
-SomeClass.classFunc() // classFunc()
-
-print(SomeClass.someProperty) // 10
-*/
+ class SomeClass {
+ 
+ static let someProperty: Int = 10
+ 
+ func someFunc() {
+ print(#function)
+ }
+ 
+ static func staticFunc() {
+ print(#function)
+ }
+ 
+ class func classFunc() {
+ print(#function)
+ }
+ 
+ }
+ 
+ let someClass = SomeClass()
+ 
+ someClass.someFunc() // someFunc()
+ 
+ SomeClass.staticFunc() // staticFunc()
+ 
+ SomeClass.classFunc() // classFunc()
+ 
+ print(SomeClass.someProperty) // 10
+ */
 
 // MARK: - Object-oriented programming (ООП)
+/*
+ 
+ class Vehicle {
+ var currentSpeed = 0.0
+ var description: String {
+ return "\(currentSpeed) км/ч"
+ }
+ }
+ 
+ class Car: Vehicle {
+ var gear = 1
+ 
+ override var description: String {
+ return super.description + " in gear \(gear)"
+ }
+ }
+ 
+ class AutomaticCar: Car {
+ override var currentSpeed: Double {
+ didSet {
+ gear = Int(currentSpeed / 10.0) + 1
+ }
+ }
+ }
+ 
+ let automaticCar = AutomaticCar()
+ automaticCar.currentSpeed = 35.0
+ print(automaticCar.currentSpeed) // 35.0
+ print(automaticCar.gear) // 4
+ print("AutomaticCar: \(automaticCar.description)") // AutomaticCar: 35.0 км/ч in gear 4
+ 
+ private func getImages(imagesStrings: String...) -> [UIImage] {
+ var imagesArray = [UIImage]()
+ for imageString in imagesStrings {
+ if let image = UIImage(named: imageString) {
+ imagesArray.append(image)
+ }
+ }
+ return imagesArray
+ }
+ */
 
-class Vehicle {
-    var currentSpeed = 0.0
-    var description: String {
-        return "\(currentSpeed) км/ч"
+
+
+// MARK: - Any & AnyObject
+
+// Any - Любой тип     AnyObject - Любой ссылочный тип (класс)
+
+/*
+var things: [Any] = []
+
+things.append(3)
+things.append("Ruslan")
+things.append({ (name: String) -> String in "Hello \(name)" })
+
+print(things) // [3, "Ruslan", (Function)]
+
+
+// Is - Проверка элемента на тип
+
+for item in things {
+    if item is String {
+        print(item) // "Ruslan"
+    } else if item is Int {
+        print(item) // 3
     }
 }
 
-class Car: Vehicle {
-    var gear = 1
-    
-    override var description: String {
-        return super.description + " in gear \(gear)"
+// As - Преборазование элемента в нужный тип
+
+for item in things {
+    if let someString = item as? String {
+        print(someString) // "Ruslan"
     }
 }
 
-class AutomaticCar: Car {
-    override var currentSpeed: Double {
-        didSet {
-            gear = Int(currentSpeed / 10.0) + 1
-        }
-    }
-}
-
-let automaticCar = AutomaticCar()
-automaticCar.currentSpeed = 35.0
-print(automaticCar.currentSpeed) // 35.0
-print(automaticCar.gear) // 4
-print("AutomaticCar: \(automaticCar.description)") // AutomaticCar: 35.0 км/ч in gear 4
-
-private func getImages(imagesStrings: String...) -> [UIImage] {
-    var imagesArray = [UIImage]()
-    for imageString in imagesStrings {
-        if let image = UIImage(named: imageString) {
-            imagesArray.append(image)
-        }
-    }
-    return imagesArray
-}
-
+// Crash
+//let someString = things[0] as! String
+//print(someString)
+*/
 
