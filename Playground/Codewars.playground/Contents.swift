2f8a355f09f57e62db3066ee87d6b62ee72739d8
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-20 04:42:23 +0300

    The codewars' task passed

--- Playground/Codewars.playground/Contents.swift
+++ Playground/Codewars.playground/Contents.swift
@@ -26,6 +26,8 @@ func summationV2(_ n: Int) -> Int {
 
 */
 
+
+
 // MARK: - Учитывая массив целых чисел в виде строк и чисел, верните сумму значений массива, как если бы все они были числами. Верните ответ в виде числа.
 
 /*
@@ -50,6 +52,30 @@ sumMix([15, 90, 2, "fdsfdsa"])
 // V2
 func sumMixV2(_ arr: [Any]) -> Int {
     return arr.reduce(0) { $0 + (Int("\($1)") ?? 0) }
+}
+
+*/
+
+
+
+// MARK: - На днях я видел потрясающее видео, где парень взломал несколько лампочек, управляемых через Wi-Fi, пролетев мимо них на дроне. Блестящий. В этом ката мы воссоздадим этот трюк... вроде того. Вам дадут две строки: lamps и drone. lamps представляет собой ряд выключенных ламп, каждая из которых представлена x. Когда эти лампы включены, они должны быть представлены o. Строка drone представляет собой положение дрона T (есть ли лучшее предложение для персонажа??) и траекторию его полета до этой точки =. Дрон всегда летит слева направо и всегда начинается в начале ряда фонарей. Куда бы ни летел дрон, включая его текущее положение, это приведет к включению лампы в этом положении. Верните получившуюся строку ламп. См. примеры тестов для большей ясности.
+
+//func basicTests() {
+//    XCTAssertEqual(flyBy(lamps: "xxxxxx", drone: "====T"), "ooooox")
+//    XCTAssertEqual(flyBy(lamps: "xxxxxxxxx", drone: "==T"), "oooxxxxxx")
+//    XCTAssertEqual(flyBy(lamps: "xxxxxxxxxxxxxxx", drone: "=========T"), "ooooooooooxxxxx")
+//}
+
+/*
+
+// V1
+func flyBy(lamps: String, drone: String) -> String {
+    return lamps.replacingOccurrences(of: "x", with: "o", range: drone.startIndex..<drone.endIndex)
+}
+
+// V2
+func flyByV2( lamps: String, drone: String) -> String {
+ String(repeating: "o", count: drone.count) + String(repeating: "x", count: (lamps.count - drone.count))
 }
 
 */
