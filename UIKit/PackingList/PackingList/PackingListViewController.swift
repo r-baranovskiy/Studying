10943762028b75417e2ceedecaac1ffcbaee7c69
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-27 21:10:34 +0300

    Project init

---
+++ UIKit/PackingList/PackingList/PackingListViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class PackingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
