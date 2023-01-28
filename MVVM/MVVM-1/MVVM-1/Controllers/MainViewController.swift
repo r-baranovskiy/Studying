461001f91f8bf3c79f6eea16544c48ea46a945e6
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-28 20:25:42 +0300

    Init train MVVM project

---
+++ MVVM/MVVM-1/MVVM-1/Controllers/MainViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        // Do any additional setup after loading the view.
+    }
+}
+
