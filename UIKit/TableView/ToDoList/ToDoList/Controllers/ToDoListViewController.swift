57d37367c63cd829c422335fe30cee97668143f5
Kashchenko <rus.baranovskiy@gmail.com>
2023-01-30 15:25:34 +0300

    Project init

---
+++ UIKit/TableView/ToDoList/ToDoList/Controllers/ToDoListViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+// MARK: - UI Constants
+
+// TableView
+private let tableView = UITableView()
+
+/// The main list that displays tasks' categories
+final class ToDoListViewController: UIViewController {
+
+    // MARK: - Lifecycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        
+    }
+    
+    private func configureTableView() {
+        
+    }
+    
+    private func addConstraints() {
+        
+    }
+}
+
