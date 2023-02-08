05a506e12b827f6fbacc0620739033a35caeaae7
koko6a <rus.baranovskiy@gmail.com>
2023-02-08 22:48:29 +0300

    mvp learning started

---
+++ UIKit/MPV-Study-Project/MPV-Study-Project/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  MPV-Study-Project
+//
+//  Created by Кащенко on 8.02.23.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let secondName: String
+}
