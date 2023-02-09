ac827288215597ba616dacaf58c640d16d8c33ed
koko6a <rus.baranovskiy@gmail.com>
2023-02-09 10:49:59 +0300

    Apple Cource UIKit was started

---
+++ UIKit/AppleCourceUIKit/AppleCourceUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleCourceUIKit
+//
+//  Created by Кащенко on 9.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
