c6e932a119d8414482c1fbeffaca3f8b19d5b403
koko6a <rus.baranovskiy@gmail.com>
2023-02-09 11:53:05 +0300

    add diffableDataSource to collection

--- UIKit/AppleCourceUIKit/AppleCourceUIKit/Application/SceneDelegate.swift
+++ UIKit/AppleCourceUIKit/AppleCourceUIKit/Application/SceneDelegate.swift
@@ -14,9 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
+        let layout = UICollectionViewFlowLayout()
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = ReminderListViewController(collectionViewLayout: layout)
         window?.makeKeyAndVisible()
     }
 }
