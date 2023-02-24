6442afc6d1666f6c1f00975bdebae470fcd2067f
koko6a <rus.baranovskiy@gmail.com>
2023-02-24 21:48:08 +0300

    add readme

--- TDD/UnitTestingLearnApp/UnitTestingLearnAppTests/UnitTestingLearnAppTests.swift
+++ TDD/UnitTestingLearnApp/UnitTestingLearnAppTests/UnitTestingLearnAppTests.swift
@@ -21,4 +21,13 @@ final class UnitTestingLearnAppTests: XCTestCase {
         // then
         XCTAssert(value == 0, "Lowest value isn't zero")
     }
+    
+    func testHighesttValueShouldBe100() {
+        // given
+        sut.setValue(with: 120)
+        // when
+        let value = sut.value
+        // then
+        XCTAssert(value == 100, "Highest value should be equal 100")
+    }
 }
