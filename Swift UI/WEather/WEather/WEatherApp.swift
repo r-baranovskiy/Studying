bf6c7072f6af3df7ae5be4d629839debe163bc26
koko6a <rus.baranovskiy@gmail.com>
2023-03-04 23:56:14 +0300

    SwiftUI project Weather init

---
+++ Swift UI/WEather/WEather/WEatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEatherApp.swift
+//  WEather
+//
+//  Created by Кащенко on 4.03.23.
+//
+
+import SwiftUI
+
+@main
+struct WEatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
