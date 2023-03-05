f06b00ff105dcfdbcbd20a2e53bb109b25b5addd
koko6a <rus.baranovskiy@gmail.com>
2023-03-06 00:55:54 +0300

    List Started completed

---
+++ Swift UI/ListStarter/ListStarter/ListStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListStarterApp.swift
+//  ListStarter
+//
+//  Created by Кащенко on 5.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ListStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            VideoListView()
+        }
+    }
+}
---
+++ Swift UI/ListStarter/ListStarter/VideoListView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct VideoListView: View {
+    var videos: [Video] = VideoList.topTen
+    
+    var body: some View {
+        NavigationView {
+            List(videos, id: \.id) { video in
+                NavigationLink(destination: VideoDetailView(video: video)) {
+                    HStack {
+                        VideoCell(video: video)
+                    }
+                }
+                .navigationTitle("Sean's Top 10")
+            }
+        }
+    }
+}
+
+struct VideoCell: View {
+    var video: Video
+    
+    var body: some View {
+        Image(video.imageName)
+            .resizable()
+            .scaledToFit()
+            .frame(height: 70)
+            .cornerRadius(4)
+            .padding(.vertical, 5)
+        VStack(alignment: .leading, spacing: 5) {
+            Text(video.title)
+                .fontWeight(.semibold)
+                .lineLimit(2)
+                .minimumScaleFactor(0.5)
+            
+            Text(video.uploadDate)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
