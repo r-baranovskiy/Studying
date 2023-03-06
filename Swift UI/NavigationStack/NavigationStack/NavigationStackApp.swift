eddac1b9bdaa2ddc5770eda2948e1e4bf92314b2
koko6a <rus.baranovskiy@gmail.com>
2023-03-06 15:46:26 +0300

    NavigationStack passed

---
+++ Swift UI/NavigationStack/NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Кащенко on 6.03.23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
