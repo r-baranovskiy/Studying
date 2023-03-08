521d9fd94fbadc62a95b656010f5b7ecc64c2700
koko6a <rus.baranovskiy@gmail.com>
2023-03-09 01:23:26 +0300

    add icon

--- Swift UI/TicTacToe/TicTacToe/ContentView.swift
+++ Swift UI/TicTacToe/TicTacToe/ContentView.swift
@@ -51,6 +51,7 @@ struct ContentView: View {
                                     in: moves)
                                 moves[computerPosition] = Move(
                                     player: .computer, boardIndex: computerPosition)
+                                isGameboardDisabled = false
                                 
                                 if checkWinCondition(for: .computer, in: moves) {
                                     alertItem = AlertContext.computerWin
@@ -61,8 +62,6 @@ struct ContentView: View {
                                     alertItem = AlertContext.draw
                                     return
                                 }
-                                
-                                isGameboardDisabled = false
                             }
                         }
                     }
@@ -86,8 +85,53 @@ struct ContentView: View {
     }
     
     private func determineComputerMovePosition(in moves: [Move?]) -> Int {
-        var movePosition = Int.random(in: 0..<9)
+        // If AI can win then win
+        let winPatterns: Set<Set<Int>> = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
+            [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+        ]
         
+        let computerMoves = moves
+            .compactMap { $0 }
+            .filter { $0.player == .computer }
+        
+        let computerPosition = Set(computerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(computerPosition)
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvailable {
+                    return winPositions.first!
+                }
+            }
+        }
+        
+        // If AI can't win, then block
+        let humanMoves = moves
+            .compactMap { $0 }
+            .filter { $0.player == .human }
+        
+        let humanPosition = Set(humanMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns {
+            let winPositions = pattern.subtracting(humanPosition)
+            if winPositions.count == 1 {
+                let isAvailable = !isSquareOccupied(in: moves, forIndex: winPositions.first!)
+                if isAvailable {
+                    return winPositions.first!
+                }
+            }
+        }
+        
+        // If AI can't block, then take middle square
+        let centerSquare = 4
+        if !isSquareOccupied(in: moves, forIndex: centerSquare) {
+            return centerSquare
+        }
+        
+        // If AI can't take middle square, take random available square
+        var movePosition = Int.random(in: 0..<9)
         while isSquareOccupied(in: moves, forIndex: movePosition) {
             movePosition = Int.random(in: 0..<9)
         }
@@ -109,7 +153,6 @@ struct ContentView: View {
         for pattern in winPatterns where pattern.isSubset(of: playerPosition) {
             return true
         }
-        
         return false
     }
     
