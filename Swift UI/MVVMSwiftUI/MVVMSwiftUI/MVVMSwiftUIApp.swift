6532d64fd96f6c25a1fde0390d95aa521a82bddd
koko6a <rus.baranovskiy@gmail.com>
2023-03-09 21:52:22 +0300

    start to mvvm passed

---
+++ Swift UI/MVVMSwiftUI/MVVMSwiftUI/MVVMSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSwiftUIApp.swift
+//  MVVMSwiftUI
+//
+//  Created by Кащенко on 9.03.23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AppetizerListView()
+        }
+    }
+}
---
+++ Swift UI/MVVMSwiftUI/MVVMSwiftUI/Views/AppetizerListView.swift
@@ -0,0 +1,33 @@
+//
+import SwiftUI
+
+struct AppetizerListView: View {
+    
+    @StateObject private var viewModel = AppetizerListViewModel()
+        
+    var body: some View {
+        ZStack {
+            NavigationView {
+                List(viewModel.appetizers, id: \.id) { appetizer in
+                    AppetizerCell(appetizer: appetizer)
+                }
+                .navigationTitle("🍟 Appetizers")
+            }
+            .onAppear { viewModel.getAppetizers() }
+            
+            if viewModel.isLoading { LoadingView() }
+        }
+        
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
+        }
+    }
+}
+
+
+struct AppetizerListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppetizerListView()
+    }
+}
